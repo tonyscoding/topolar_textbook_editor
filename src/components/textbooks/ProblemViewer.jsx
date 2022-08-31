66f55f72b419bbcabc0cf641604942c3f68fd847
qoquma <euichan1108@gmail.com>
2022-08-31 15:34:19 +0900

    fix problem bug

--- src/components/textbooks/ProblemViewer.jsx
+++ src/components/textbooks/ProblemViewer.jsx
@@ -1,12 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import Loader from "@/components/guideComponents/Loader";
-import {Dropdown, Option} from "../guideComponents/Dropdown";
+import { Dropdown } from "@nextui-org/react";
 import useApi from "../../apis/useApi";
 import {getProblemList} from "../../apis/apiServices";
 import ProblemContent from "./contents/ProblemContent";
 
 export const ProblemViewer = ({getProblemApi, getProblemListApi, inputRef}) => {
-    const [select, setSelect] = useState();
+    const [select, setSelect] = useState(new Set(["default"]));
     const [listLoading, setListLoading] = useState(true);
     const [listResolved, setListResolved] = useState();
     const [problemLoading, setProblemLoading] = useState(true);
@@ -15,8 +15,14 @@ export const ProblemViewer = ({getProblemApi, getProblemListApi, inputRef}) => {
     const [_, __, listCallback] = getProblemListApi;
     const [___, ____, problemCallback] = getProblemApi;
 
+
+    const selectedValue = React.useMemo(
+        () => Array.from(select).join(", ").replaceAll("_", " "),
+        [select]
+    );
+
     useEffect(() => {
-        console.log("useeffect")
+        console.log("useeffect", selectedValue)
         listCallback()
             .then(r => {
                 console.log(r);
@@ -27,13 +33,16 @@ export const ProblemViewer = ({getProblemApi, getProblemListApi, inputRef}) => {
     },[])
 
     useEffect(() => {
-        console.log("problemViewer", inputRef);
-        problemCallback(inputRef.current)
-            .then(r => {
-                console.log("problem", r);
-                setProblemLoading(false);
-                setProblemResolved(r);
-            })
+        if(selectedValue!=="default") {
+            console.log("problemViewer", inputRef);
+            problemCallback(selectedValue)
+                .then(r => {
+                    console.log("problem", r);
+                    setProblemLoading(false);
+                    setProblemResolved(r);
+                })
+        }
+
     },[JSON.stringify(select)]);
 
     useEffect(() => {
@@ -50,24 +59,32 @@ export const ProblemViewer = ({getProblemApi, getProblemListApi, inputRef}) => {
                     </div>
                 ) : (
                     <>
-                        <Dropdown
-                            className={"problem-dropdown"}
-                            value={select}
-                            onChange={(value)=>{
-                                console.log("value",value)
-                                inputRef.current=value.value;
-                                setSelect(value);
-                                setProblemLoading(true);
-                            }}
-                            placeholder="선택해 주세요"
-                            font="body-2"
-                        >
-                        {
-                            listResolved.map((problem, idx) => (
 
-                                        <Option value={problem.number}>{problem.number} {problem.title}</Option>
-                            ))
-                        }
+                        <Dropdown>
+                            <Dropdown.Button flat color="secondary" css={{ tt: "capitalize" }}>
+                                {select}
+                            </Dropdown.Button>
+                            <Dropdown.Menu
+                                aria-label="Single selection actions"
+                                color="secondary"
+                                disallowEmptySelection
+                                selectionMode="single"
+                                selectedKeys={select}
+                                onSelectionChange={(value)=>{
+                                    console.log("value",value)
+                                    inputRef.current=Array.from(value).join(", ").replaceAll("_", " "),
+                                    setSelect(value);
+                                    setProblemLoading(true);
+                                }}
+                            >
+                                <Dropdown.Item key={"default"}>선택하세요</Dropdown.Item>
+                                {
+                                    listResolved.map((problem, idx) => (
+
+                                        <Dropdown.Item key={problem.number}>{problem.number} {problem.title}</Dropdown.Item>
+                                    ))
+                                }
+                            </Dropdown.Menu>
                         </Dropdown>
                         {
                             problemLoading ? (
