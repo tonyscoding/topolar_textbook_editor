a1f582780a7fbb1f1792da234c1f4b1c008bb0c6
PRESSO <caff1nepill@gmail.com>
2022-09-01 14:04:24 +0900

    change version

--- src/components/textbooks/TextbookSidebar.jsx
+++ src/components/textbooks/TextbookSidebar.jsx
@@ -170,7 +170,7 @@ const TextbookSidebar = ({
 
             <hr />
 
-            <div style={{fontSize: 20, marginLeft: 10}}>v 0.10.1</div>
+            <div style={{fontSize: 20, marginLeft: 10}}>v 0.10.2</div>
         </div>
     )
 
